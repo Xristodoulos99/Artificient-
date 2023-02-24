f76bb64cdba9b1a2472d74f3f5b6ccfd9de00d77
Xristodoulos99 <56251014+Xristodoulos99@users.noreply.github.com>
2023-02-24 19:57:21 +0200

    Add files via upload

---
+++ React/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import React from 'react';
+
+function App() {
+  return (
+     <React.Fragment>
+        <Navbar/>
+        <Body/>
+        <Footer/>
+     </React.Fragment>
+  );
+}
+
+export default App;
